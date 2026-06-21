799670225fe4f2f6bf92007e713657c0232748b1
rockethalen18 <rockethalen18@users.noreply.github.com>
2026-06-21 18:47:03 -0500

    fix: add basePath back for GitHub Pages

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: "/atlantic-optical",
+  assetPrefix: "/atlantic-optical/",
   trailingSlash: true,
   turbopack: {},
 };
